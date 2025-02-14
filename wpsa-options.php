79c0fe8bda354a0600d7c9202ab2105dcc1495ee
jmf3658 <jfullmer@austin.utexas.edu>
2025-02-14 13:20:15 -0700

    Clean syntax

--- wpsa-options.php
+++ wpsa-options.php
@@ -5,51 +5,51 @@
  */
 
 function utexas_wpsax_filter_option($value, $option_name) {
-	$defaults = [
-		'connection_type' => 'internal',
-		'auto_provision'         => false,
-		'permit_wp_login'        => true,
-		'get_user_by'            => 'login',
-		'user_login_attribute'   => 'username',
-		'user_email_attribute'   => 'Email',
-		'display_name_attribute' => 'full_name',
-		'first_name_attribute' => 'full_name',
-		'default_role'           => get_option('default_role'),
-		'internal_config' => [
-			'strict'       => true,
-			'debug'        => true,
-			'baseurl'      => home_url(),
-			'sp'           => [
-				'entityId' => home_url() . '/onelogin',
-				'assertionConsumerService' => [
-					'url'  => wp_login_url(),
-					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+  $defaults = [
+    'connection_type' => 'internal',
+    'auto_provision'         => false,
+    'permit_wp_login'        => true,
+    'get_user_by'            => 'login',
+    'user_login_attribute'   => 'username',
+    'user_email_attribute'   => 'Email',
+    'display_name_attribute' => 'full_name',
+    'first_name_attribute' => 'full_name',
+    'default_role'           => get_option('default_role'),
+    'internal_config' => [
+      'strict'       => true,
+      'debug'        => true,
+      'baseurl'      => home_url(),
+      'sp'           => [
+        'entityId' => home_url() . '/onelogin',
+        'assertionConsumerService' => [
+          'url'  => wp_login_url(),
+          'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+        ],
+        'x509cert' => file_get_contents(ABSPATH . 'wp-content/uploads/private/saml/assets/cert/sp-cert.crt'),
+        'privateKey' => file_get_contents(ABSPATH . 'wp-content/uploads/private/saml/assets/cert/sp-key.pem'),
+      ],
+      'idp'          => [
+        'entityId' => 'https://enterprise-test.login.utexas.edu/idp/shibboleth',
+        'singleSignOnService' => [
+          'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+          'url' => 'https://enterprise-test.login.utexas.edu/idp/profile/SAML2/Redirect/SSO',
 				],
-				'x509cert' => file_get_contents(ABSPATH . 'wp-content/uploads/private/saml/assets/cert/sp-cert.crt'),
-				'privateKey' => file_get_contents(ABSPATH . 'wp-content/uploads/private/saml/assets/cert/sp-key.pem'),
-			],
-			'idp'          => [
-				'entityId' => 'https://enterprise-test.login.utexas.edu/idp/shibboleth',
-				'singleSignOnService' => array(
-					'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-					'url' => 'https://enterprise-test.login.utexas.edu/idp/profile/SAML2/Redirect/SSO',
-				),
-				'singleLogoutService' => [
-					'https://enterprise.login.utexas.edu/idp/profile/Logout'
-				],
-				'x509cert' => file_get_contents(ABSPATH . 'wp-content/uploads/private/saml/assets/cert/idp-cert-test.crt'),
-				'certFingerprint' => '',
-				'certFingerprintAlgorithm' => '',
-		  ],
+        'singleLogoutService' => [
+          'https://enterprise.login.utexas.edu/idp/profile/Logout'
+        ],
+        'x509cert' => file_get_contents(ABSPATH . 'wp-content/uploads/private/saml/assets/cert/idp-cert-test.crt'),
+        'certFingerprint' => '',
+        'certFingerprintAlgorithm' => '',
+      ],
       'security' => [
         'allowRepeatAttributeName' => true,
-		  ],
-		],
+      ],
+    ],
     'security' => [
       'allowRepeatAttributeName' => true,
-		],
+    ],
   ];
 
-	$value = isset($defaults[$option_name]) ? $defaults[$option_name] : $value;
-	return $value;
+  $value = isset($defaults[$option_name]) ? $defaults[$option_name] : $value;
+  return $value;
 }
